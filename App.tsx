9adb49574318803ea0962375e33f2ecdd0dec1dd
Talisson-b99 <talissonb422@gmail.com>
2023-12-29 04:03:09 -0300

    chore: react native base

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -11,20 +12,13 @@ export default function App() {
     Roboto_700Bold,
   });
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#202024",
-      }}
-    >
+    <NativeBaseProvider>
       <StatusBar
         barStyle={"light-content"}
         backgroundColor="transparent"
         translucent
       />
-      {fontLoaded ? <Text>Hello World!</Text> : <View />}
-    </View>
+      {fontLoaded ? <View /> : <View />}
+    </NativeBaseProvider>
   );
 }
