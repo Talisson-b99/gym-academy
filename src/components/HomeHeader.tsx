c7dc1826314c4ade2949fabd0ce4a5ddc42aee08
Talisson-b99 <talissonb422@gmail.com>
2023-12-30 02:33:34 -0300

    feat: add home header component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,12 +1,28 @@
-import { HStack, Heading, Text, VStack } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+import { HStack, Heading, Text, VStack, Icon } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
-    <HStack>
-      <VStack>
-        <Text color="gray.100">Olá</Text>
-        <Heading color="gray.100">Talisson</Heading>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        size={16}
+        source={{ uri: "https://github.com/Talisson-b.png" }}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Talisson
+        </Heading>
       </VStack>
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
     </HStack>
   );
 }
