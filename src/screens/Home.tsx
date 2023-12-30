5e470c3213809d5495c4e5c7b2503b3baf89ca1b
Talisson-b99 <talissonb422@gmail.com>
2023-12-30 02:59:17 -0300

    feat: add group component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,15 @@
-import { VStack } from "native-base";
+import { HStack, VStack } from "native-base";
 import { HomeHeader } from "../components/HomeHeader";
+import { Group } from "../components/Group";
 
 export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <HStack>
+        <Group name="costas" />
+        <Group name="ombro" />
+      </HStack>
     </VStack>
   );
 }
