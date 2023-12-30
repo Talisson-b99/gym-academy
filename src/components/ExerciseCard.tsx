466992d98ac9a87c8390f5642dc7637eccef00cf
Talisson-b99 <talissonb422@gmail.com>
2023-12-30 04:27:51 -0300

    feat: add exerciseCard component

---
+++ src/components/ExerciseCard.tsx
@@ -0,0 +1,42 @@
+import { HStack, Heading, Image, Text, VStack, Icon } from "native-base";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+type Props = TouchableOpacityProps & {};
+
+export function ExerciseCard({ ...rest }: Props) {
+  return (
+    <TouchableOpacity {...rest}>
+      <HStack
+        bg="gray.500"
+        alignItems="center"
+        p={2}
+        pr={4}
+        rounded="md"
+        mb={3}
+      >
+        <Image
+          source={{
+            uri: "https://www.origym.com.br/midia/remada-unilateral-3.jpg",
+          }}
+          alt="exercíco remada unilateral"
+          w={16}
+          h={16}
+          mr={4}
+          rounded="md"
+          resizeMode="cover"
+        />
+
+        <VStack flex={1}>
+          <Heading color="white" fontSize="lg">
+            Remada unilateral
+          </Heading>
+          <Text color="gray.200" fontSize="sm" mt={1} numberOfLines={2}>
+            3 séries x 12 repitições
+          </Text>
+        </VStack>
+        <Icon as={Entypo} name="chevron-thin-right" color="gray.300" size={6} />
+      </HStack>
+    </TouchableOpacity>
+  );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,18 @@
-import { HStack, VStack, FlatList } from "native-base";
-import { HomeHeader } from "../components/HomeHeader";
-import { Group } from "../components/Group";
 import { useState } from "react";
+import { HStack, VStack, FlatList, Heading, Text } from "native-base";
+
+import { Group } from "../components/Group";
+import { HomeHeader } from "../components/HomeHeader";
+import { ExerciseCard } from "../components/ExerciseCard";
 
 export function Home() {
   const [groups, setGroups] = useState(["costa", "ombro", "bíceps", "tríceps"]);
+  const [exercises, setExercises] = useState([
+    "Puxada frontal",
+    "Remada curvada",
+    "Remada unilateral",
+    "Levantamento terra",
+  ]);
   const [groupSelected, setGroupSelected] = useState("costa");
 
   return (
@@ -26,6 +34,23 @@ export function Home() {
           />
         )}
       />
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ marginBottom: 20 }}
+          renderItem={({ item }) => <ExerciseCard />}
+        />
+      </VStack>
     </VStack>
   );
 }
